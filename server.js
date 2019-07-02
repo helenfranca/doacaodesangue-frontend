ace92df55a18e427ca564717fd525ab1fff6a7a2
Your Name <you@example.com>
2019-07-02 19:33:09 -0300

    Deploy 19:33

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-
-// app.use(express.static(__dirname + "/dist/doacaodesangue-frontend"));
-
-// app.get("/*", (req, res) => {
-//   res.sendFile(
-//     path.join(__dirname + "/dist/doacaodesangue-frontend/index.html")
-//   );
-//   console.log("caminho:", __dirname);
-// });
-
-// app.listen(process.env.PORT || 8080);
-
 const express = require("express");
-const app = express();
 const path = require("path");
-app.use(express.static(__dirname + "/doacaodesangue-frontend"));
-app.listen(process.env.PORT || 8080); //PATH LOCATION STARTEGY
-app.get("/*", function(req, res) {
-  const fullPath = path.join(__dirname + "/doacaodesangue-frontend/index.html");
-  console.log(" Fetching from.." + fullPath);
-  res.sendFile(fullPath);
+const app = express();
+
+app.use(express.static(__dirname + "/dist/doacaodesangue-frontend"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname + "/dist/doacaodesangue-frontend/index.html")
+  );
+  console.log("caminho:", __dirname);
 });
-console.log("Server started running.."); //Changed to run on Heroku
+
+app.listen(process.env.PORT || 8080);
