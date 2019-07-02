3f67502f7d6f30e7e6cb9beae5609db8e2e60e95
landsalexandre <landerson.lma@hotmail.com>
2019-07-02 10:17:57 -0300

    Index

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require("express");
-const path = require("path");
+/* const express = require('express');
+const path = require('path');
 const app = express();
+ 
+app.use(express.static(__dirname+'/dist/doacaodesangue-frontend'));
+ 
+app.get('/*', function (req, res) {
 
-app.use(express.static(__dirname + "/dist/src/doacaodesangue-frontend"));
-
-app.get("/*", function(req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/src/doacaodesangue-frontend/index.html")
-  );
+  res.sendFile(path.join(__dirname + '/dist/doacaodesangue-frontend/index.html'));
 });
 
+app.listen(process.env.PORT || 8080); */
+
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+ 
 app.listen(process.env.PORT || 8080);
