8d6297cc6a00b35b6a017e0c8d490fd432bc12a3
Your Name <you@example.com>
2019-07-02 20:08:18 -0300

    Deploy 20:08

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //API : 'http://localhost:3000/'
-  API : 'http://doacaosangue.herokuapp.com/'
+  API: "http://doacaodesangue.herokuapp.com/"
 };
 
 /*
