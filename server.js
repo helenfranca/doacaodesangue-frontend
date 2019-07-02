06c833ceba07b88d266bc856a80bc56c5219cc5b
Your Name <you@example.com>
2019-07-02 18:39:23 -0300

    Deploy 18:39

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.static(__dirname + "/dist/doacaodesangue-frontend"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/src/index.html"));
+  res.sendFile(
+    path.join(__dirname + "/dist/doacaodesangue-frontend/index.html")
+  );
+  console.log(__dirname);
 });
 
 app.listen(process.env.PORT || 8080);
