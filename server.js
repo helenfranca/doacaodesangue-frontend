5533944504e0c20e44da8952894865028c3c0239
Your Name <you@example.com>
2019-07-02 19:24:52 -0300

    Deploy 19:24

--- server.js
+++ server.js
@@ -16,12 +16,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(__dirname + "/dist/doacaodesangue-frontend"));
+app.use(express.static(__dirname + "/doacaodesangue-frontend"));
 app.listen(process.env.PORT || 8080); //PATH LOCATION STARTEGY
 app.get("/*", function(req, res) {
-  const fullPath = path.join(
-    __dirname + "/dist/doacaodesangue-frontend/index.html"
-  );
+  const fullPath = path.join(__dirname + "/doacaodesangue-frontend/index.html");
   console.log(" Fetching from.." + fullPath);
   res.sendFile(fullPath);
 });
