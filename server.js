620272d884e3416587685ac364eb4e142b3334d5
landsalexandre <landerson.lma@hotmail.com>
2019-07-02 10:27:53 -0300

    package

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ app.listen(process.env.PORT || 8080); */
 
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(__dirname + '/dist/doacaodesangue-frontend'));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(__dirname +'/dist/doacaodesangue-frontend/index.html'));
 });
  
 app.listen(process.env.PORT || 8080);
